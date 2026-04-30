59fa7ca618548f81653bcc839e76143c8d3ff63e
Ankush Karki <ankushdhojkarki@gmail.com>
2026-04-30 13:04:55 +0545

    chore: migrate database from sqlite3 to postgresql

--- core/settings.py
+++ core/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfolio_db',
+        'USER': 'ankush',
+        'PASSWORD': 'ankush',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
