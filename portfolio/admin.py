531d0c49e41ad23ffe849b9494ca74a88eb10dd8
Ankush Karki <ankushdhojkarki@gmail.com>
2026-05-03 20:42:19 +0545

    blog model chanegs

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project, Skill, ContactMessage
+from .models import Project, Skill, ContactMessage, Quote
 
 # Register your models here.
-admin.site.register([Project, Skill, ContactMessage])
+admin.site.register([Project, Skill, ContactMessage, Quote])
