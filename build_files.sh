9ab75d6b019d0233c119b8ff145aa3bbae73e48a
Ankush Karki <ankushdhojkarki@gmail.com>
2026-05-07 11:05:54 +0545

    Fix: set PYTHONPATH to vercel packages location in build script

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python manage.py collectstatic --noinput --clear
-python manage.py migrate
+PYTHONPATH=/vercel/path0/.vercel_python_packages python manage.py collectstatic --noinput --clear
+PYTHONPATH=/vercel/path0/.vercel_python_packages python manage.py migrate
