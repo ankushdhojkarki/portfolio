d9a0c9e95fd12fb4eceeb280b83fcdd981ffed9d
Ankush Karki <ankushdhojkarki@gmail.com>
2026-05-14 14:30:35 +0545

    add viewsets and routers

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'api/projects', views.ProjectViewSet, basename = 'project')
+router.register(r'api/skills', views.SkillViewSet, basename='skill')
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
     path('api/quote/', views.random_quote, name='random_quote'),
-    path('api/projects/', views.project_list, name='projectlist_api'),
-    path('api/skill/', views.skill_list, name='skilllist_api'),
-    path('api/projects/<int:pk>/', views.project_detail, name='project_detail'),
-]
+] + router.urls
--- portfolio/views.py
+++ portfolio/views.py
@@ -8,11 +8,11 @@ import logging
 import random
 from core import settings
 from .models import Project, Skill, ContactMessage, Quote
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from .serializers import ProjectSerializer, SkillSerializer
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
+from rest_framework import viewsets
 
 logger = logging.getLogger(__name__)
 
@@ -60,48 +60,11 @@ def random_quote(request):
         return JsonResponse({'text': q.text, 'author': q.author})
     return JsonResponse({'text': 'Let the beauty of what you love be what you do.', 'author': 'Rumi'})
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def project_list(request):
-    if request.method == 'GET':
-        projects = Project.objects.all()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-@permission_classes([IsAuthenticated])
-def project_detail(request, pk):
-    try:
-        project = Project.objects.get(pk=pk)
-    except Project.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = ProjectSerializer(project)
-        return Response(serializer.data)
-    
-    if request.method in ['PUT', 'PATCH']:
-        is_partial = (request.method == 'PATCH')
-        serializer = ProjectSerializer(project, data=request.data, partial = is_partial)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE':
-        project.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-
-@api_view(['GET',])
-def skill_list(request):
-    skills = Skill.objects.all()
-    serializer = SkillSerializer(skills, many=True)
-    return Response(serializer.data)
+class SkillViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
